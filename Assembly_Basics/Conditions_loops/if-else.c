4eb605912fc1e3ae61015078f58fb18dd392c1ba
piero <piero@gmail.com>
2020-02-07 20:17:47 +0100

    condizionali e loop

--- Assembly_Basics/Conditions_loops/if-else.c
+++ Assembly_Basics/Conditions_loops/if-else.c
@@ -2,10 +2,14 @@
 int main(int argc, char** argv)
 {
 	int x = 5;
-	int y = 5;
+	int y = 3;
 	if(x == y)
 	{
 		printf("%d equals %d", x, y);
+	}
+	else
+	{
+		printf("%d not equal to %d", x, y);
 	}
 	return 0;
 }
---
+++ Assembly_Basics/Conditions_loops/loop.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc, char** argv)
+{
+	for(int i = 0; i < 15; i++)
+	{
+		printf("this is loop %d\n", i);
+	}
+}
